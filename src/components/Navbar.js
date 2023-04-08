583c42f0df9a3b0d6d8b9666dea6a7be310c6def
Reggeditt <tkodjosarso@gmail.com>
2023-04-08 11:32:53 +0000

    replace navlink anchor tag with react Navlink

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,5 @@
+import { NavLink } from 'react-router-dom';
+
 const Navbar = () => {
   const links = [
     { name: 'Books', path: '/' },
@@ -8,7 +10,7 @@ const Navbar = () => {
       <h1>Bookstore CMS</h1>
       <ul className="nav-links">
         {links.map((link) => (
-          <li key={link.name}><a href={link.path}>{link.name}</a></li>
+          <li key={link.name}><NavLink to={link.path}>{link.name}</NavLink></li>
         ))}
       </ul>
     </nav>
