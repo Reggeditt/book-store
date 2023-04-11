14c0a0a9689015ef87b85ba61c8276207b876faf
Reggeditt <tkodjosarso@gmail.com>
2023-04-11 22:30:01 +0000

    connect redux to react

--- src/components/addBook.js
+++ src/components/addBook.js
@@ -1,16 +1,37 @@
-const AddBook = () => (
-  <form className="add-book-form">
-    <span>ADD NEW BOOK</span>
-    <input type="text" placeholder="Book title" />
-    <select>
-      <option value="Action">Action</option>
-      <option value="Biography">Biography</option>
-      <option value="History">History</option>
-      <option value="Fiction">Fiction</option>
-      <option value="Romance">Romance</option>
-    </select>
-    <input type="submit" value="Add Book" />
-  </form>
-);
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/booksSlice';
+
+const AddBook = () => {
+  const dispatch = useDispatch();
+
+  const getNewBook = (e) => {
+    e.preventDefault();
+    const newBook = {
+      id: Math.floor(Math.random() * 100),
+      title: e.target[0].value,
+      category: e.target[1].value,
+      author: 'Unknown',
+    };
+    dispatch(addBook(newBook));
+  };
+
+  return (
+    <form
+      className="add-book-form"
+      onSubmit={(e) => getNewBook(e)}
+    >
+      <span>ADD NEW BOOK</span>
+      <input type="text" placeholder="Book title" />
+      <select>
+        <option value="Action">Action</option>
+        <option value="Biography">Biography</option>
+        <option value="History">History</option>
+        <option value="Fiction">Fiction</option>
+        <option value="Romance">Romance</option>
+      </select>
+      <input type="submit" value="Add Book" />
+    </form>
+  );
+};
 
 export default AddBook;
--- src/components/bookList.js
+++ src/components/bookList.js
@@ -1,7 +1,20 @@
+import { useSelector } from 'react-redux';
 import BookItem from './bookItem';
 
-const BooksList = ({ books }) => (
-  books.map((book) => <BookItem key={book.id} book={JSON.stringify(book)} />)
-);
+const BooksList = () => {
+  const booksArray = useSelector((state) => state.books);
+
+  return (
+    <div>
+      {
+        booksArray.books.map((book) => (
+          <div key={book.id}>
+            <BookItem book={JSON.stringify(book)} />
+          </div>
+        ))
+      }
+    </div>
+  );
+};
 
 export default BooksList;
