18fd5ad2f22dfdb116eb5b04b432ff5ffa2eb223
Reggeditt <tkodjosarso@gmail.com>
2023-04-13 07:31:25 +0000

    update form data collection method

--- src/components/addBook.js
+++ src/components/addBook.js
@@ -1,37 +1,48 @@
 import { v4 as uuid } from 'uuid';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
 import { addBook } from '../redux/books/booksSlice';
 
 const AddBook = () => {
   const dispatch = useDispatch();
-
+  const categories = useSelector((state) => state.categories.categories);
+  const [author, setAuthor] = useState('');
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
   const getNewBook = (e) => {
     e.preventDefault();
     const newBook = {
       id: uuid(),
-      title: e.target[0].value,
-      category: e.target[1].value,
-      author: e.target[2].value,
+      title,
+      category,
+      author,
     };
+    console.log(newBook);
     dispatch(addBook(newBook));
   };
 
   return (
-    <form
-      className="add-book-form"
-      onSubmit={(e) => getNewBook(e)}
-    >
+    <form className="add-book-form">
       <span>ADD NEW BOOK</span>
-      <input type="text" placeholder="Book title" />
+      <input onChange={(e) => setTitle(e.target.value)} type="text" placeholder="Book title" />
       <select>
-        <option value="Action">Action</option>
-        <option value="Biography">Biography</option>
-        <option value="History">History</option>
-        <option value="Fiction">Fiction</option>
-        <option value="Romance">Romance</option>
+        { categories.map((category) => (
+          <option
+            onSelect={(e) => setCategory(e.target.value)}
+            key={category}
+            value={category}
+          >
+            {category}
+          </option>
+        ))}
       </select>
-      <input type="name" placeholder="Author" />
-      <button type="submit">Add Book</button>
+      <input onChange={(e) => setAuthor(e.target.value)} type="name" placeholder="Author" />
+      <button
+        type="submit"
+        onClick={(e) => getNewBook(e)}
+      >
+        Add Book
+      </button>
     </form>
   );
 };
