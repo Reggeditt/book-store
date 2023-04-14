8bc8228d3e6378bb196986cc768b7bcd37d4b6f3
Reggeditt <tkodjosarso@gmail.com>
2023-04-14 23:54:53 +0000

    add styles to bookItem components

--- src/components/bookItem.js
+++ src/components/bookItem.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBook, deleteBook } from '../redux/books/booksSlice';
+import './bookItem.css';
 
 const BookItem = ({ book, id }) => {
   const dispatch = useDispatch();
@@ -13,6 +14,7 @@ const BookItem = ({ book, id }) => {
         <div className="book-item__author">{JSON.parse(book).author}</div>
         <div className="book-details-btns">
           <button type="button" className="book-item__comment">Comments</button>
+          <div className="line-divide" />
           <button
             type="button"
             className="book-item__remove"
@@ -24,16 +26,20 @@ const BookItem = ({ book, id }) => {
           >
             Remove
           </button>
+          <div className="line-divide" />
           <button type="button" className="book-item__edit">Edit</button>
         </div>
       </div>
       <div className="book-item__status">
-        <div className="Oval-2" />
+        <div className="Oval-2">
+          <div className="Oval-1" />
+        </div>
         <div className="status-data">
           <span className="status-data__title">64%</span>
           <span className="status-data__completed">Completed</span>
         </div>
       </div>
+      <div className="line-divide Line-2" />
       <div className="book-item__progress">
         <div className="progress-title">CURRENT CHAPTER</div>
         <div className="progress-details">Chapter 17</div>
